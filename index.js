99f70bf314253c32b59d0af4ca1f3afd7d909794
nickzap872 <167074229+nickzap872@users.noreply.github.com>
2024-06-19 18:47:33 -0400

    Add event listener to input#button element

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    // Grab the element with id 'button'
+    const input = document.getElementById('button');
+  
+    // Define the event listener function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener to the element
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to activate the event listener
+  addingEventListener();
+  
